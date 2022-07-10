c10957e965996665be51d183ce5b0d7db1b341e7
xSeri98 <102629371+xSeri98@users.noreply.github.com>
2022-07-10 22:50:25 +0800

    Add files via upload

---
+++ bitdegree.js
@@ -0,0 +1,15 @@
+"use strict";
+console.log("hey");
+
+let ToBeReversed = "tae"
+
+console.log(`Here is some word for you: ${ToBeReversed.slice(2,3)}`);
+
+ let MyArray =["hello","hi"];
+
+console.log(MyArray[1]);
+
+let myorder = 5;
+myorder+=20
+console.log(myorder);
+
---
+++ test.cs
@@ -0,0 +1,9 @@
+using System;
+
+public class Try{
+ public static void main(string[] args){
+    Console.WriteLine("hey");
+
+ }
+
+}
